b83078d26fd9dba16172e3a8fe6d95e88ea5d211
Wouter Scherphof <wsf@MBP-van-Wouter.local>
2015-04-01 12:02:36 +0200

    func NewAccount

--- account.go
+++ account.go
@@ -18,8 +18,12 @@ type PWD struct {
   Value string
 }
 
-func NewPWD (pwd1, pwd2 string) (pwd PWD, err error) {
-  if pwd1 != pwd2 {
+func NewPWD (r *http.Request) (pwd PWD, err error) {
+  // TODO: further validation, password hashing, ...
+  pwd1, pwd2 := r.FormValue("pwd1"), r.FormValue("pwd2")
+  if pwd1 == "" {
+    err = errors.New("Password empty")
+  } else if pwd1 != pwd2 {
     err = errors.New("Passwords not equal")
   } else {
     pwd = PWD{
@@ -40,29 +44,35 @@ type Account struct {
   LastName string
 }
 
+func NewAccount (r *http.Request, pwd string) (account Account, err error) {
+  // TODO: further validation, ...
+  account = Account{
+    Created: time.Now(),
+    UID: r.FormValue("uid"),
+    PWD: pwd,
+    Country: r.FormValue("country"),
+    Postcode: r.FormValue("postcode"),
+    FirstName: r.FormValue("firstname"),
+    LastName: r.FormValue("lastname"),
+  }
+  return
+}
+
 func SignUp (w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-  if pwd, err := NewPWD(r.FormValue("pwd1"), r.FormValue("pwd2")); err != nil {
+  if pwd, err := NewPWD(r); err != nil {
     Error(w, r, ps, err)
   } else if res, err := db.Insert(PWD_TABLE, pwd); err != nil {
     Error(w, r, ps, err)
   } else if len(res.GeneratedKeys) != 1 {
     Error(w, r, ps, errors.New("Failed saving password"))
+  } else if account, err := NewAccount(r, res.GeneratedKeys[0]); err != nil {
+    Error(w, r, ps, err)
+  // TODO: unique key on UID
+  } else if _, err := db.Insert(ACCOUNT_TABLE, account); err != nil {
+    Error(w, r, ps, err)
   } else {
-    // TODO: further validation, unique key on UID, password hashing, confirmation email, ...
-    record := Account{
-      Created: time.Now(),
-      UID: r.FormValue("uid"),
-      PWD: res.GeneratedKeys[0],
-      Country: r.FormValue("country"),
-      Postcode: r.FormValue("postcode"),
-      FirstName: r.FormValue("firstname"),
-      LastName: r.FormValue("lastname"),
-    }
-    if _, err := db.Insert(ACCOUNT_TABLE, record); err != nil {
-      Error(w, r, ps, err)
-    } else {
-      w.WriteHeader(http.StatusOK)
-      w.Write([]byte("account created"))
-    }
+    // TODO: confirmation email, formatted response, ...
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("account created"))
   }
 }
