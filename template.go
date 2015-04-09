4684e4bd426c475763729fea286a0eb635cd804e
Wouter Scherphof <wsf@MBP-van-Wouter.local>
2015-04-09 13:27:31 +0200

    Too bad lang still has to be a parameter to Msg
    Oh, and the app is still broken ;-)

--- template.go
+++ template.go
@@ -9,26 +9,26 @@ import (
   "github.com/wscherphof/msg"
 )
 
+var aceOptions = &ace.Options{ // var, no const, since the compiler says this literal isn't a constant
+  BaseDir: "templates",
+  FuncMap: template.FuncMap{
+    "Msg": msg.Msg,
+  },
+}
+
 func T (base string, inner string, data map[string]interface{}) func (http.ResponseWriter, *http.Request, httprouter.Params) {
+  if data == nil {
+    data = map[string]interface{}{}
+  }
   return func (w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
     language := msg.Language(r.Header.Get("Accept-Language"))
     if inner == "lang" {
       inner = base + "-" + language.Main
     }
-    // TODO: make the options a const
-    tpl, err := ace.Load(base, inner, &ace.Options{
-      BaseDir: "templates",
-      // TODO: maybe move the Msg function to data to get it to enclose the language parameter again
-      FuncMap: template.FuncMap{
-        "Msg": msg.Msg,
-      },
-    })
+    tpl, err := ace.Load(base, inner, aceOptions)
     if err != nil {
       fmt.Println("ace.Load: ", err)
       return
-    }
-    if data == nil {
-      data = map[string]interface{}{}
     }
     data["lang"] = language
     err = tpl.Execute(w, data)
