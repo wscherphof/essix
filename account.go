70d6eb9da4982d550d3399768e66fdfe93aceb7c
Wouter Scherphof <wsf@MBP-van-Wouter.local>
2015-04-19 22:25:44 +0200

    First shot to email (and config in db)

--- account.go
+++ account.go
@@ -5,6 +5,9 @@ import (
   "github.com/julienschmidt/httprouter"
   "github.com/wscherphof/expeertise/data"
   "github.com/wscherphof/expeertise/model"
+  "net/smtp"
+  "github.com/wscherphof/expeertise/db"
+  "log"
 )
 
 func SignUpForm (w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
@@ -23,6 +26,33 @@ func SignUp (w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
     }
   } else {
     // TODO: confirmation email, formatted response, ...
+    const CONFIG_TABLE string = "config"
+    const EMAIL_PWD string = "EMAIL_PWD"
+    type pwdType struct{
+      Key string
+      PWD string
+    }
+    opts := db.NewTableCreateOpts()
+    opts.PrimaryKey = "Key"
+    if cursor, _ := db.TableCreate(CONFIG_TABLE, opts); cursor != nil {
+      log.Println("INFO: table created:", CONFIG_TABLE)
+    }
+    pwd := new(pwdType)
+    if err, found := db.Get(CONFIG_TABLE, EMAIL_PWD, pwd); err != nil {
+      Error(w, r, ps, err)
+    } else if !(found) {
+      db.Insert(CONFIG_TABLE, &pwdType{
+        Key: EMAIL_PWD,
+        PWD: "",
+      })
+      log.Println("INFO: record created:", EMAIL_PWD, "in table:", CONFIG_TABLE)
+    }
+    //
+    auth := smtp.PlainAuth("", "expeertise@gmail.com", pwd.PWD, "smtp.gmail.com")
+    msg := []byte("Subject: Test email from Go!\nThis is the email body.")
+    if err := smtp.SendMail("smtp.gmail.com:587", auth, "expeertise@gmail.org", []string{account.UID}, msg); err != nil {
+      Error(w, r, ps, err)
+    }
     w.WriteHeader(http.StatusOK)
     w.Write([]byte("account created: " + account.UID))
   }
