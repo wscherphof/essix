3c07862b00dbc4d058acfcbb1f8344da3ced2026
Wouter Scherphof <wsf@MBP-van-Wouter.local>
2015-04-21 20:05:42 +0200

    template to string

--- template.go
+++ template.go
@@ -7,6 +7,8 @@ import (
   "github.com/yossi/ace"
   "github.com/julienschmidt/httprouter"
   "github.com/wscherphof/msg"
+  "bytes"
+  "io"
 )
 
 var aceOptions = &ace.Options{ // var, no const, since the compiler says this literal isn't a constant
@@ -16,11 +18,11 @@ var aceOptions = &ace.Options{ // var, no const, since the compiler says this li
   },
 }
 
-func T (base string, inner string, data map[string]interface{}) httprouter.Handle {
+func t (base string, inner string, data map[string]interface{}) (func(io.Writer, *http.Request)) {
   if data == nil {
     data = map[string]interface{}{}
   }
-  return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+  return func(w io.Writer, r *http.Request) {
     language := msg.Language(r.Header.Get("Accept-Language"))
     data["lang"] = language
     if inner == "lang" {
@@ -31,5 +33,19 @@ func T (base string, inner string, data map[string]interface{}) httprouter.Handl
     } else if err := tpl.Execute(w, data); err != nil {
       log.Panicln("ERROR: tpl.Execute:", err)
     }
+  }
+}
+
+func T (base string, inner string, data map[string]interface{}) httprouter.Handle {
+  return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    t(base, inner, data)(w, r)
+  }
+}
+
+func TS (base string, inner string, data map[string]interface{}) (func(*http.Request)(string)) {
+  var b *bytes.Buffer
+  return func(r *http.Request) (string) {
+    t(base, inner, data)(b, r)
+    return string(b.Bytes())
   }
 }
