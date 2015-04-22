934aa201f0a60c147454a558032a58ef854ca0df
Wouter Scherphof <wsf@MBP-van-Wouter.local>
2015-04-23 00:09:01 +0200

    messages moved to their proper package

---
+++ messages.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "github.com/wscherphof/msg"
+)
+
+func DefineMessages () {
+  var m, a = msg.Init()
+
+  m("")
+  a("nl", "")
+  a("en", "")
+
+  m("Hi")
+  a("nl", "Hoi")
+  a("en", "Hi")
+
+  m("Take me home")
+  a("nl", "Naar de startpagina")
+  a("en", "To the home page")
+}
