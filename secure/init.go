0264031422c5ca5939034254c4757e4400c4b67c
Wouter Scherphof <wsf@MBP-van-Wouter.local>
2015-05-18 23:39:19 +0200

    sendEmail() in separate file

--- secure/init.go
+++ secure/init.go
@@ -1,12 +1,8 @@
 package secure
 
 import (
-  "net/http"
   "github.com/wscherphof/secure"
-  "github.com/wscherphof/msg"
-  "github.com/wscherphof/expeertise/util"
   "github.com/wscherphof/expeertise/model/account"
-  "github.com/wscherphof/expeertise/email"
 )
 
 func Init () {
@@ -19,28 +15,4 @@ func Init () {
     }
     return
   })
-}
-
-func sendEmail (r *http.Request, acc *account.Account, resource, code, extra string) (err error, remark string) {
-  subject := msg.Msg(r)(resource + " subject")
-  scheme := "http"
-  if r.TLS != nil {
-    scheme = "https"
-  }
-  path := scheme + "://" + r.Host + "/account/" + resource + "/" + acc.UID
-  // TODO: format links as "buttons" instead of hyperlinks
-  body := util.BTemplate(resource + "_email", "lang", map[string]interface{}{
-    "action": path + "?code=" + code + "&extra=" + string(util.URLEncode([]byte(extra))),
-    "cancel": path,
-    "name": acc.Name(),
-    "extra": extra,
-  })(r)
-  if e := email.Send(subject, string(body), acc.UID); e != nil {
-    if e == email.ErrNotSentImmediately {
-      remark = e.Error()
-    } else {
-      err = e
-    }
-  }
-  return
 }
---
+++ secure/sendEmail.go
@@ -0,0 +1,33 @@
+package secure
+
+import (
+  "net/http"
+  "github.com/wscherphof/msg"
+  "github.com/wscherphof/expeertise/util"
+  "github.com/wscherphof/expeertise/model/account"
+  "github.com/wscherphof/expeertise/email"
+)
+
+func sendEmail (r *http.Request, acc *account.Account, resource, code, extra string) (err error, remark string) {
+  subject := msg.Msg(r)(resource + " subject")
+  scheme := "http"
+  if r.TLS != nil {
+    scheme = "https"
+  }
+  path := scheme + "://" + r.Host + "/account/" + resource + "/" + acc.UID
+  // TODO: format links as "buttons" instead of hyperlinks
+  body := util.BTemplate(resource + "_email", "lang", map[string]interface{}{
+    "action": path + "?code=" + code + "&extra=" + string(util.URLEncode([]byte(extra))),
+    "cancel": path,
+    "name": acc.Name(),
+    "extra": extra,
+  })(r)
+  if e := email.Send(subject, string(body), acc.UID); e != nil {
+    if e == email.ErrNotSentImmediately {
+      remark = e.Error()
+    } else {
+      err = e
+    }
+  }
+  return
+}
