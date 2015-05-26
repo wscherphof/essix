4d84d7d5600b37c15eb86c7c759fbf199ab0c97f
Wouter Scherphof <wsf@MacBook-Pro.local>
2015-05-26 13:58:41 +0200

    channel instead of new goroutines + retry Get
    So, yes, quite tricky indeed

--- captcha/init.go
+++ captcha/init.go
@@ -8,6 +8,8 @@ import (
 )
 
 const CAPTCHA_TABLE string = "captcha"
+const CAPTCHA_BUF int = 100
+const CAPTCHA_TRIES int = 3
 const TIMEOUT time.Duration = 15 * time.Minute
 
 type captchaType struct{
@@ -18,27 +20,40 @@ type captchaType struct{
 
 type store struct{}
 
-func (s *store) Set (id string, digits []byte) {
-  // Wonder if this will ever prove just a bit too tricky
-  go func() {
-    if _, err := db.Insert(CAPTCHA_TABLE, &captchaType{
-      ID: id,
-      Digits: digits,
-      Created: time.Now().Unix(),
-    }); err != nil {
+var captchaChannel chan *captchaType
+
+func setHandler () {
+  for item := range captchaChannel {
+    if _, err := db.Insert(CAPTCHA_TABLE, item); err != nil {
       log.Println("ERROR: Insert failed in table " + CAPTCHA_TABLE + ":", err)
     }
-  }()
+  }
+}
+
+func (s *store) Set (id string, digits []byte) {
+  // Wonder if this will ever prove just a bit too tricky
+  captchaChannel <- &captchaType{
+    ID: id,
+    Digits: digits,
+    Created: time.Now().Unix(),
+  }
 }
 
 func (s *store) Get (id string, clear bool) (digits []byte) {
   c := new(captchaType)
-  if err, found := db.Get(CAPTCHA_TABLE, id, c); err != nil {
-    log.Println("ERROR: Get failed in table " + CAPTCHA_TABLE + ":", err)
-  } else if ! found {
-    log.Println("ERROR: Not found in table " + CAPTCHA_TABLE + ":", err)
-  } else {
-    digits = c.Digits
+  for try := 0; try < CAPTCHA_TRIES; try++ {
+    if err, found := db.Get(CAPTCHA_TABLE, id, c); err != nil {
+      log.Println("ERROR: Get failed in table " + CAPTCHA_TABLE + ":", err)
+    } else if ! found {
+      log.Println("ERROR: Not found in table " + CAPTCHA_TABLE + ":", id)
+    } else {
+      digits = c.Digits
+      break
+    }
+    time.Sleep(time.Duration(try + 1) * time.Second)
+  }
+  if len(digits) == 0 {
+    log.Println("DEBUG: GET FAILED!!!")
   }
   return
 }
@@ -46,6 +61,7 @@ func (s *store) Get (id string, clear bool) (digits []byte) {
 var Server = captcha.Server(captcha.StdWidth, captcha.StdHeight)
 
 func Init () {
+  DefineMessages()
   if cursor, _ := db.TableCreate(CAPTCHA_TABLE); cursor != nil {
     log.Println("INFO: table created:", CAPTCHA_TABLE)
     if _, err := db.IndexCreate(CAPTCHA_TABLE, "Created"); err != nil {
@@ -54,8 +70,9 @@ func Init () {
       log.Println("INFO: index created:", CAPTCHA_TABLE)
     }
   }
+  captchaChannel = make(chan *captchaType, CAPTCHA_BUF)
+  go setHandler()
   captcha.SetCustomStore(new(store))
-  DefineMessages()
   go func() {
     for {
       limit := time.Now().Unix()
