52f891aec72d6592135b74d03a11602088f66672
Wouter Scherphof <wsf@MacBook-Pro.local>
2015-05-29 20:29:07 +0200

    middleware -> secure/init

--- secure/secure.go
+++ secure/secure.go
@@ -1,9 +1,25 @@
 package secure
 
 import (
+  "net/http"
   "github.com/wscherphof/secure"
+  "github.com/wscherphof/secure/httprouter/middleware"
   "github.com/wscherphof/expeertise/model/account"
 )
+
+var UpdateAuthentication = secure.UpdateAuthentication
+
+var Authenticate = middleware.Authenticate
+
+var IfAuthenticate = middleware.IfAuthenticate
+
+func Authentication (r *http.Request) (ret *account.Account) {
+  if auth := middleware.Authentication(r); auth != nil {
+    acc := auth.(account.Account)
+    ret = &acc
+  }
+  return
+}
 
 func Init () {
   DefineMessages()
