ec9f3958874681c2502f83e1fb020e8ef3aa33ae
Wouter Scherphof <wsf@MacBook-Pro.local>
2015-06-02 00:13:28 +0200

    error.go -> router.go

--- router/router.go
+++ router/router.go
@@ -9,6 +9,21 @@ import (
 
 var Router = httprouter.New()
 
+type Error struct{
+  Error error
+  Conflict bool
+  Tail string
+  Data map[string]interface{}
+}
+
+func NewError (e error, tail ...string) (err *Error) {
+  err = &Error{Error: e}
+  if len(tail) > 0 {
+    err.Tail = tail[0]
+  }
+  return
+}
+
 type ErrorHandle func(http.ResponseWriter, *http.Request, httprouter.Params)(*Error)
 
 func ErrorHandleFunc(errorHandle ErrorHandle) (handle httprouter.Handle) {
