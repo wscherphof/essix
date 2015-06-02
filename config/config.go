3f15c5bc3836269fe6f1a38652d6c5e0163380fe
Wouter Scherphof <wsf@MacBook-Pro.local>
2015-06-02 23:33:24 +0200

    cosmetic

--- config/config.go
+++ config/config.go
@@ -6,25 +6,27 @@ import (
 	"log"
 )
 
-const CONFIG_TABLE = "config"
-const CONFIG_PK = "Key"
+const (
+	table = "config"
+	pk    = "Key"
+)
 
 func init() {
-	if cursor, _ := db.TableCreatePK(CONFIG_TABLE, CONFIG_PK); cursor != nil {
-		log.Println("INFO: table created:", CONFIG_TABLE)
+	if cursor, _ := db.TableCreatePK(table, pk); cursor != nil {
+		log.Println("INFO: table created:", table)
 	}
 }
 
 func Get(key string, result interface{}) (err error) {
-	if e, found := db.Get(CONFIG_TABLE, key, result); e != nil {
+	if e, found := db.Get(table, key, result); e != nil {
 		err = e
 	} else if !(found) {
-		err = errors.New("Key " + key + " not found in table " + CONFIG_TABLE)
+		err = errors.New("Key " + key + " not found in table " + table)
 	}
 	return
 }
 
 func Set(record interface{}) (err error) {
-	_, err = db.InsertUpdate(CONFIG_TABLE, record)
+	_, err = db.InsertUpdate(table, record)
 	return
 }
