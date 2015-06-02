a923b3c7c0c7de9ed33a48c875c06b991fc3ef21
Wouter Scherphof <wsf@MacBook-Pro.local>
2015-06-02 15:24:14 +0200

    go fmt

--- main.go
+++ main.go
@@ -1,49 +1,49 @@
 package main
 
 import (
-  "net/http"
-  "github.com/gorilla/handlers"
-  "github.com/gorilla/context"
-  "github.com/wscherphof/expeertise/router"
-  "github.com/wscherphof/expeertise/secure"
-  "github.com/wscherphof/expeertise/captcha"
-  "log"
-  "os"
+	"github.com/gorilla/context"
+	"github.com/gorilla/handlers"
+	"github.com/wscherphof/expeertise/captcha"
+	"github.com/wscherphof/expeertise/router"
+	"github.com/wscherphof/expeertise/secure"
+	"log"
+	"net/http"
+	"os"
 )
 
 const (
-  // TODO: flags/envvars
-  HTTP_HOST  = "localhost"
-  HTTP_PORT  = ":9090"
-  HTTPS_PORT = ":10443"
+	// TODO: flags/envvars
+	HTTP_HOST  = "localhost"
+	HTTP_PORT  = ":9090"
+	HTTPS_PORT = ":10443"
 )
 
-func main () {
-  router.GET    ("/", secure.IfSecureHandle(
-    router.Template("home", "home_loggedin", nil),
-    router.Template("home", "home_loggedout", nil)))
-  
-  router.Router.Handler("GET", "/captcha/*filepath", captcha.Server)
-  router.Router.ServeFiles("/static/*filepath", http.Dir("./static"))
+func main() {
+	router.GET("/", secure.IfSecureHandle(
+		router.Template("home", "home_loggedin", nil),
+		router.Template("home", "home_loggedout", nil)))
 
-  go func(){
-    address := HTTP_HOST + HTTP_PORT
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-      http.Redirect(w, r, "https://" + address + r.URL.Path, http.StatusMovedPermanently)
-    })
-    log.Println("INFO: HTTP  server @", address)
-    log.Fatal(http.ListenAndServe(address,
-      handlers.CombinedLoggingHandler(os.Stdout,
-    http.DefaultServeMux)))
-  }()
+	router.Router.Handler("GET", "/captcha/*filepath", captcha.Server)
+	router.Router.ServeFiles("/static/*filepath", http.Dir("./static"))
 
-  address := HTTP_HOST + HTTPS_PORT
-  log.Println("INFO: HTTPS server @", address)
-  log.Fatal(http.ListenAndServeTLS(address, "cert.pem", "key.pem",
-    context.ClearHandler(
-    secure.AuthenticationHandler(
-    handlers.HTTPMethodOverrideHandler(
-    handlers.CompressHandler(
-    handlers.CombinedLoggingHandler(os.Stdout, 
-  router.Router)))))))
+	go func() {
+		address := HTTP_HOST + HTTP_PORT
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			http.Redirect(w, r, "https://"+address+r.URL.Path, http.StatusMovedPermanently)
+		})
+		log.Println("INFO: HTTP  server @", address)
+		log.Fatal(http.ListenAndServe(address,
+			handlers.CombinedLoggingHandler(os.Stdout,
+				http.DefaultServeMux)))
+	}()
+
+	address := HTTP_HOST + HTTPS_PORT
+	log.Println("INFO: HTTPS server @", address)
+	log.Fatal(http.ListenAndServeTLS(address, "cert.pem", "key.pem",
+		context.ClearHandler(
+			secure.AuthenticationHandler(
+				handlers.HTTPMethodOverrideHandler(
+					handlers.CompressHandler(
+						handlers.CombinedLoggingHandler(os.Stdout,
+							router.Router)))))))
 }
--- messages.go
+++ messages.go
@@ -1,53 +1,53 @@
 package main
 
 import (
-  "github.com/wscherphof/msg"
+	"github.com/wscherphof/msg"
 )
 
-func init () {
-  var m, a = msg.Definition()
+func init() {
+	var m, a = msg.Definition()
 
-  m("")
-  a("nl", "")
-  a("en", "")
+	m("")
+	a("nl", "")
+	a("en", "")
 
-  m("Hi")
-  a("nl", "Hoi")
-  a("en", "Hi")
+	m("Hi")
+	a("nl", "Hoi")
+	a("en", "Hi")
 
-  m("Take me home")
-  a("nl", "Naar de startpagina")
-  a("en", "To the home page")
+	m("Take me home")
+	a("nl", "Naar de startpagina")
+	a("en", "To the home page")
 
-  m("Try again")
-  a("nl", "Opnieuw proberen")
-  a("en", "Try again")
+	m("Try again")
+	a("nl", "Opnieuw proberen")
+	a("en", "Try again")
 
-  m("Time format")
-  a("nl", "2-1-2006, 15.04 u. (MST)")
-  a("en", "1/2/2006 at 3:04pm (MST)")
+	m("Time format")
+	a("nl", "2-1-2006, 15.04 u. (MST)")
+	a("en", "1/2/2006 at 3:04pm (MST)")
 
-  m("Last modified")
-  a("nl", "Laatste wijziging")
-  a("en", "Last modified")
+	m("Last modified")
+	a("nl", "Laatste wijziging")
+	a("en", "Last modified")
 
-  m("Save changes")
-  a("nl", "Wijzigingen opslaan")
-  a("en", "Save changes")
+	m("Save changes")
+	a("nl", "Wijzigingen opslaan")
+	a("en", "Save changes")
 
-  m("Current")
-  a("nl", "Huidig")
-  a("en", "Current")
+	m("Current")
+	a("nl", "Huidig")
+	a("en", "Current")
 
-  m("New")
-  a("nl", "Nieuw")
-  a("en", "New")
+	m("New")
+	a("nl", "Nieuw")
+	a("en", "New")
 
-  m("change")
-  a("nl", "wijzigen")
-  a("en", "change")
+	m("change")
+	a("nl", "wijzigen")
+	a("en", "change")
 
-  m("Confirm")
-  a("nl", "Bevestigen")
-  a("en", "Confirm")
+	m("Confirm")
+	a("nl", "Bevestigen")
+	a("en", "Confirm")
 }
