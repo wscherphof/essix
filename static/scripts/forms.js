070cbfdb6b711c2bc03622b7a9fd93b3b810caf9
Wouter Scherphof <wsf@MacBook-Pro.local>
2015-06-11 13:15:33 +0200

    align + flow

--- static/scripts/forms.js
+++ static/scripts/forms.js
@@ -1,39 +1,24 @@
-function max(list) {
-	var max = 0;
-	if (list) {
-		for (var i = 0; i < list.length; i++) {
-			var elem = list[i];
-			var val = elem.offsetLeft + elem.offsetWidth;
-			if (val > max) {
-				max = val;
-			}
-		}
-	}
-	return max;
-}
-
-function position(list, max) {
-	if (list) {
-		for (var i = 0; i < list.length; i++) {
-			var elem = list[i];
-			var offset = max - elem.offsetLeft - elem.offsetWidth;
-			elem.style.position = "relative";
-			elem.style.left = offset + "px";
-		}
-	}
-}
-
 function alignRight(event) {
 	var envelopes = document.getElementsByClassName("envelope");
 	if (!envelopes) return;
 	for (var i = 0; i < envelopes.length; i++) {
 		var envelope = envelopes[i];
 		var rights = envelope.querySelectorAll('.right');
-		var right2s = envelope.querySelectorAll('.right2');
-		var maxRights = max(rights);
-		var maxRight2s = max(right2s);
-		position(rights, maxRights);
-		position(right2s, maxRight2s);
+		if (!rights) continue;
+		var max = 0;
+		for (var i = 0; i < rights.length; i++) {
+			var elem = rights[i];
+			var val = elem.offsetLeft + elem.offsetWidth;
+			if (val > max) {
+				max = val;
+			}
+		}
+		for (var i = 0; i < rights.length; i++) {
+			var elem = rights[i];
+			var offset = max - elem.offsetLeft - elem.offsetWidth;
+			elem.style.position = "relative";
+			elem.style.left = offset + "px";
+		}
 	}
 }
 
