f3b573ba9400a74e80487be94fac15cf7dfdf438
Wouter Scherphof <wsf@MacBook-Pro.local>
2015-06-24 11:04:06 +0200

    SUSPICIOUS

--- ratelimit/ratelimit.go
+++ ratelimit/ratelimit.go
@@ -22,32 +22,6 @@ const (
 	table = "ratelimit"
 )
 
-type path string
-
-type client struct {
-	IP       string
-	Clear    time.Time
-	Requests map[path]time.Time
-}
-
-func getClient(ip string) (c *client) {
-	c = new(client)
-	err, found := db.Get(table, ip, c)
-	if err != nil {
-		log.Printf("WARNING: error getting from table %v", table)
-	}
-	if !found {
-		c.IP = ip
-		c.Requests = make(map[path]time.Time)
-	}
-	return
-}
-
-func (c *client) save() (err error) {
-	_, err = db.InsertUpdate(table, c)
-	return
-}
-
 func init() {
 	if _, err := db.TableCreatePK(table, "IP"); err == nil {
 		log.Println("INFO: table created:", table)
@@ -55,10 +29,16 @@ func init() {
 	secure.RegisterRequestTokenData(token{})
 }
 
+type path string
+
 type token struct {
 	IP        string
 	Path      path
 	Timestamp time.Time
+}
+
+func ip(r *http.Request) string {
+	return strings.Split(r.RemoteAddr, ":")[0]
 }
 
 func NewToken(r *http.Request) (string, error) {
@@ -69,8 +49,30 @@ func NewToken(r *http.Request) (string, error) {
 	})
 }
 
-func ip(r *http.Request) string {
-	return strings.Split(r.RemoteAddr, ":")[0]
+type requests map[path]time.Time
+
+type client struct {
+	IP       string
+	Clear    time.Time
+	Requests requests
+}
+
+func getClient(ip string) (c *client) {
+	c = new(client)
+	err, found := db.Get(table, ip, c)
+	if err != nil {
+		log.Printf("WARNING: error getting from table %v: %v", table, err)
+	}
+	if !found {
+		c.IP = ip
+		c.Requests = make(requests)
+	}
+	return
+}
+
+func (c *client) save() (err error) {
+	_, err = db.InsertUpdate(table, c)
+	return
 }
 
 func Handle(seconds int, handle router.ErrorHandle) router.ErrorHandle {
@@ -80,30 +82,31 @@ func Handle(seconds int, handle router.ErrorHandle) router.ErrorHandle {
 		if rate := r.FormValue("_rate"); rate == "" {
 			err = router.NewError(ErrInvalidRequest)
 			err.Conflict = true
-			log.Printf("ATTACK: rate limit token missing %#v", r)
+			log.Printf("SUSPICIOUS: rate limit token missing %v %v", ip, p)
 		} else if e := secure.RequestToken(rate).Read(t); e != nil {
 			err = router.NewError(ErrInvalidRequest)
 			err.Conflict = true
-			log.Printf("ATTACK: rate limit token unreadable %#v", r)
+			log.Printf("SUSPICIOUS: rate limit token unreadable %v %v", ip, p)
 		} else if t.IP != ip {
 			err = router.NewError(ErrInvalidRequest)
 			err.Conflict = true
-			log.Printf("ATTACK: rate limit token invalid address: %v, not %v", t.IP, ip)
+			log.Printf("SUSPICIOUS: rate limit token invalid address: %v, expected %v %v", t.IP, ip, p)
 		} else if t.Path != p {
 			err = router.NewError(ErrInvalidRequest)
 			err.Conflict = true
-			log.Printf("ATTACK: rate limit token invalid path: %v, not %v", t.Path, p)
-		} else if c := getClient(ip); c.Requests[p].After(time.Now().Add(-window)) {
-			err = router.NewError(ErrTooManyRequests)
-			err.Conflict = true
-		} else if c.Requests[p].After(t.Timestamp) {
+			log.Printf("SUSPICIOUS: rate limit token invalid path: %v, token path %v, expected %v", ip, t.Path, p)
+		} else if c := getClient(ip); c.Requests[p].After(t.Timestamp) {
 			err = router.NewError(ErrTokenExpired)
+			err.Conflict = true
+			log.Printf("SUSPICIOUS: rate limit token reuse: %v %v, token %v, previous request %v", ip, p, t.Timestamp, c.Requests[p])
+		} else if c.Requests[p].After(time.Now().Add(-window)) {
+			err = router.NewError(ErrTooManyRequests)
 			err.Conflict = true
 		} else {
 			c.Requests[p] = time.Now()
 			c.Clear = time.Now().Add(window)
 			if e := c.save(); e != nil {
-				log.Printf("WARNING: error saving to table %v", table)
+				log.Printf("WARNING: error saving to table %v: %v", table, e)
 			}
 			err = handle(w, r, ps)
 		}
