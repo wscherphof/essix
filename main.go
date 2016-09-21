c5bd1e322ed33531a9446ee6e8345221a97bace3
Wouter Scherphof <wouter.scherphof@gmail.com>
2016-09-21 23:15:54 +0200

    w/o localhost https

--- main.go
+++ main.go
@@ -12,19 +12,20 @@ import (
 )
 
 var (
-	applicationAddress = env.Get("HTTP_HOST") + env.Get("HTTPS_PORT")
+	// applicationAddress = env.Get("HTTP_HOST") + env.Get("HTTPS_PORT")
+	applicationAddress = ":80"
 	redirectAddress    = env.Get("HTTP_HOST") + env.Get("HTTP_PORT")
 )
 
 func main() {
 	// Redirect server serves http & redirects everything to the application server's https address
-	go func() {
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			http.Redirect(w, r, "https://"+applicationAddress+r.URL.Path, http.StatusMovedPermanently)
-		})
-		log.Println("INFO: starting redirect server    @", redirectAddress)
-		log.Fatal(http.ListenAndServe(redirectAddress, nil))
-	}()
+	// go func() {
+	// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 		http.Redirect(w, r, "https://"+applicationAddress+r.URL.Path, http.StatusMovedPermanently)
+	// 	})
+	// 	log.Println("INFO: starting redirect server    @", redirectAddress)
+	// 	log.Fatal(http.ListenAndServe(redirectAddress, nil))
+	// }()
 
 	router.GET("/", secure.IfSecureHandle(
 		router.Template(".", "home", "home_loggedin", nil),
@@ -33,7 +34,8 @@ func main() {
 	router.Router.ServeFiles("/static/*filepath", http.Dir("./static"))
 
 	log.Println("INFO: starting application server @", applicationAddress)
-	log.Fatal(http.ListenAndServeTLS(applicationAddress, "cert.pem", "key.pem",
+	// log.Fatal(http.ListenAndServeTLS(applicationAddress, "cert.pem", "key.pem",
+	log.Fatal(http.ListenAndServe(applicationAddress,
 		context.ClearHandler(
 			handlers.HTTPMethodOverrideHandler(
 				handlers.CompressHandler(
