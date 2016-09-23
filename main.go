74afe40b849281c9be914cbc4cd5b4bf1f07bf7a
Wouter Scherphof <wouter.scherphof@gmail.com>
2016-09-23 10:57:42 +0200

    letsencrypt

--- main.go
+++ main.go
@@ -3,29 +3,15 @@ package main
 import (
 	"github.com/gorilla/context"
 	"github.com/gorilla/handlers"
-	"github.com/wscherphof/expeertise/env"
 	"github.com/wscherphof/expeertise/router"
 	"github.com/wscherphof/expeertise/secure"
+	"github.com/wscherphof/letsencrypt"
 	"log"
 	"net/http"
 	"os"
 )
 
-var (
-	// applicationAddress = env.Get("HTTP_HOST") + env.Get("HTTPS_PORT")
-	applicationAddress = ":80"
-	redirectAddress    = env.Get("HTTP_HOST") + env.Get("HTTP_PORT")
-)
-
 func main() {
-	// Redirect server serves http & redirects everything to the application server's https address
-	// go func() {
-	// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 		http.Redirect(w, r, "https://"+applicationAddress+r.URL.Path, http.StatusMovedPermanently)
-	// 	})
-	// 	log.Println("INFO: starting redirect server    @", redirectAddress)
-	// 	log.Fatal(http.ListenAndServe(redirectAddress, nil))
-	// }()
 
 	router.GET("/", secure.IfSecureHandle(
 		router.Template(".", "home", "home_loggedin", nil),
@@ -33,9 +19,13 @@ func main() {
 
 	router.Router.ServeFiles("/static/*filepath", http.Dir("./static"))
 
-	log.Println("INFO: starting application server @", applicationAddress)
-	// log.Fatal(http.ListenAndServeTLS(applicationAddress, "cert.pem", "key.pem",
-	log.Fatal(http.ListenAndServe(applicationAddress,
+	var m letsencrypt.Manager
+	if err := m.CacheFile("/appdata/letsencrypt.cache"); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("INFO: starting application server")
+	log.Fatal(m.Serve(
 		context.ClearHandler(
 			handlers.HTTPMethodOverrideHandler(
 				handlers.CompressHandler(
