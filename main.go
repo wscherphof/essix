66a047d7bba5f0b4e8ab029c68af443ed91f7a78
Wouter Scherphof <wouter.scherphof@gmail.com>
2016-09-25 00:58:12 +0200

    letsencrypt cache in init()

--- main.go
+++ main.go
@@ -24,26 +24,25 @@ func copy(src, dst string) error {
 	return nil
 }
 
+func init() {
+	if _, err := os.Stat("/appdata/letsencrypt.cache"); os.IsNotExist(err) {
+		if err := copy("/letsencrypt.cache", "/appdata/letsencrypt.cache"); err != nil {
+			log.Fatal("ERROR: ", err)
+		}
+	}
+}
+
 func main() {
+	var m letsencrypt.Manager
+	if err := m.CacheFile("/appdata/letsencrypt.cache"); err != nil {
+		log.Fatal(err)
+	}
 
 	router.GET("/", secure.IfSecureHandle(
 		router.Template(".", "home", "home_loggedin", nil),
 		router.Template(".", "home", "home_loggedout", nil)))
 
 	router.Router.ServeFiles("/static/*filepath", http.Dir("./static"))
-
-	if _, err := os.Stat("/appdata/letsencrypt.cache"); os.IsNotExist(err) {
-		if err := copy("/letsencrypt.cache", "/appdata/letsencrypt.cache"); err != nil {
-			log.Fatal("ERROR: ", err)
-		}
-	} else {
-		log.Println("INFO: using /appdata/letsencrypt.cache")
-	}
-
-	var m letsencrypt.Manager
-	if err := m.CacheFile("/appdata/letsencrypt.cache"); err != nil {
-		log.Fatal(err)
-	}
 
 	log.Println("INFO: starting application server")
 	log.Fatal(m.Serve(
