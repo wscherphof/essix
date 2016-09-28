49908cfdbd3fd1d4cace738f2b05616e2ac965e9
Wouter Scherphof <wouter.scherphof@gmail.com>
2016-09-28 22:16:53 +0200

    Add -> Set

--- messages/ratelimit.go
+++ messages/ratelimit.go
@@ -5,19 +5,19 @@ import (
 )
 
 func init() {
-	msg.New("ErrTooManyRequests").
-		Add("nl", `Sorry; hier geldt een frequentielimiet; je kan dit maar eens in
+	msg.Key("ErrTooManyRequests").
+		Set("nl", `Sorry; hier geldt een frequentielimiet; je kan dit maar eens in
       de zoveel tijd aanvragen.
       Probeer het later opnieuw.`).
-		Add("en", `Sorry; a rate limit is in effect for this request type.
+		Set("en", `Sorry; a rate limit is in effect for this request type.
       Please try again later.`)
 
-	msg.New("ErrInvalidRequest").
-		Add("nl", `Sorry; er klopt iets niet in het kader van de frequentielimiet
+	msg.Key("ErrInvalidRequest").
+		Set("nl", `Sorry; er klopt iets niet in het kader van de frequentielimiet
       die voor dit verzoek van kracht is.`).
-		Add("en", `Sorry; something is wrong with the rate limit controls.`)
+		Set("en", `Sorry; something is wrong with the rate limit controls.`)
 
-	msg.New("Limit is set to").
-		Add("nl", `De limiet is ingesteld op:`).
-		Add("en", `Rate limit is set to:`)
+	msg.Key("Limit is set to").
+		Set("nl", `De limiet is ingesteld op:`).
+		Set("en", `Rate limit is set to:`)
 }
