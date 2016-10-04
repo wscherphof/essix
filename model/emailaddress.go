6fbfba21519b7de10eda3fb444c01448ebed9581
Wouter Scherphof <wouter.scherphof@gmail.com>
2016-10-04 12:07:46 +0200

    model/account split out

---
+++ model/emailaddress.go
@@ -0,0 +1,35 @@
+package model
+
+func (a *Account) CreateEmailAddressCode(newUID string) error {
+	a.NewUID = newUID
+	a.EmailAddressCode = NewCode()
+	return a.Update(a)
+}
+
+func (a *Account) ClearEmailAddressCode(code string) (err error) {
+	if a.EmailAddressCode == code {
+		a.NewUID = ""
+		a.EmailAddressCode = ""
+		err = a.Update(a)
+	}
+	return
+}
+
+func (a *Account) ChangeEmailAddress(code string) (err error, conflict bool) {
+	uid := a.ID
+	if acc, e, c := getAccount(uid); e != nil {
+		err, conflict = e, c
+	} else if acc.EmailAddressCode == "" {
+		err, conflict = ErrCodeUnset, true
+	} else if code == "" || code != acc.EmailAddressCode {
+		err, conflict = ErrCodeIncorrect, true
+	} else {
+		acc.ID = acc.NewUID
+		if e := acc.ClearEmailAddressCode(code); e != nil {
+			err, conflict = e, false
+		} else {
+			*a = *acc
+		}
+	}
+	return
+}
