dfa4d205bd6ec099d781593550bab6f4df31b446
Wouter Scherphof <wouter.scherphof@gmail.com>
2016-10-12 00:08:25 +0200

    preliminary PRG

--- router/router.go
+++ router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/julienschmidt/httprouter"
+	"net/http"
 )
 
 var Router = httprouter.New()
@@ -13,3 +14,17 @@ func DELETE(path string, handle httprouter.Handle)  { Router.DELETE(path, handle
 func HEAD(path string, handle httprouter.Handle)    { Router.HEAD(path, handle) }
 func OPTIONS(path string, handle httprouter.Handle) { Router.OPTIONS(path, handle) }
 func PATCH(path string, handle httprouter.Handle)   { Router.PATCH(path, handle) }
+
+func Redirect(w http.ResponseWriter, r *http.Request, data map[string]string) {
+	var query string
+	if data != nil {
+		for key, value := range data {
+		    query += key + "=" + value + "&"
+		}		
+	}
+	var path = r.URL.Path + "/prg"
+	if len(query) > 0 {
+		path += "?" + query
+	}
+	http.Redirect(w, r, path, http.StatusSeeOther)
+}
--- secure/account.go
+++ secure/account.go
@@ -5,6 +5,7 @@ import (
 	"github.com/wscherphof/essix/model"
 	"github.com/wscherphof/essix/ratelimit"
 	"github.com/wscherphof/essix/template"
+	"github.com/wscherphof/essix/router"
 	"net/http"
 )
 
@@ -19,17 +20,27 @@ func NewAccountForm(w http.ResponseWriter, r *http.Request, ps httprouter.Params
 }
 
 func NewAccount(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	if account, err, conflict := model.NewAccount(r.FormValue("email"),
-		r.FormValue("pwd1"), r.FormValue("pwd2"),
-	); err != nil {
-		template.Error(w, r, err, conflict)
-	} else if err, remark := activateEmail(r, account); err != nil {
-		template.Error(w, r, err, false)
-	} else {
+	switch r.Method {
+	case "GET":
 		template.Run(w, r, "account", "NewAccount", "", map[string]interface{}{
-			"id":     account.ID,
-			"remark": remark,
+			"id":     r.FormValue("id"),
+			"remark": r.FormValue("remark"),
 		})
+	case "POST":
+		if account, err, conflict := model.NewAccount(
+			r.FormValue("email"),
+			r.FormValue("pwd1"),
+			r.FormValue("pwd2"),
+		); err != nil {
+			template.Error(w, r, err, conflict)
+		} else if err, remark := activateEmail(r, account); err != nil {
+			template.Error(w, r, err, false)
+		} else {
+			router.Redirect(w, r, map[string]string{
+				"id":     account.ID,
+				"remark": remark,
+			})
+		}
 	}
 }
 
